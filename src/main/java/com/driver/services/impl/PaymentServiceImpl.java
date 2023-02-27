25b281cc96c8e932e78066d3ed589fd99c249955
Pritam <spritam675@gmail.com>
2023-02-27 14:11:02 +0530

    checking

--- src/main/java/com/driver/services/impl/PaymentServiceImpl.java
+++ src/main/java/com/driver/services/impl/PaymentServiceImpl.java
@@ -19,26 +19,52 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public Payment pay(Integer reservationId, int amountSent, String mode) throws Exception {
-        Payment payment=new Payment();
-        Reservation reservation=reservationRepository2.findById(reservationId).get();
-        int hour=reservation.getNumberOfHours();
-        Spot spot=reservation.getSpot();
-        int mul=spot.getPricePerHour();
-        int total=hour*mul;
-        if (amountSent<total){
+//        Payment payment=new Payment();
+//        Reservation reservation=reservationRepository2.findById(reservationId).get();
+//        int hour=reservation.getNumberOfHours();
+//        Spot spot=reservation.getSpot();
+//        int mul=spot.getPricePerHour();
+//        int total=hour*mul;
+//        if (amountSent<total){
+//            throw new Exception("Insufficient Amount");
+//        }
+//        mode=mode.toUpperCase();
+//        if((!mode.equals(PaymentMode.CARD)) ||(!mode.equals(PaymentMode.UPI)) || (!mode.equals(PaymentMode.CASH))){
+//            throw new Exception("Payment mode not detected");
+//        }
+//        if(mode.equals(PaymentMode.CARD)){
+//            payment.setPaymentMode(PaymentMode.CARD);
+//        }else if(mode.equals(PaymentMode.UPI)){
+//            payment.setPaymentMode(PaymentMode.UPI);
+//        }else if(mode.equals(PaymentMode.CASH)){
+//            payment.setPaymentMode(PaymentMode.CASH);
+//        }
+//        else{
+//            throw new Exception("Payment mode not detected");
+//        }
+//        payment.setPaymentCompleted(true);
+//        payment.setReservation(reservation);
+//        reservation.setPayment(payment);
+//        reservation.getSpot().setOccupied(true);
+//        reservationRepository2.save(reservation);
+//        return payment;
+        Reservation reservation = reservationRepository2.findById(reservationId).get();
+        Payment payment = new Payment();  //This is throwing null because i didn't set payment  while adding eveything in payment
+        String payment1 = "CASH",payment2 = "CARD",payment3 = "UPI";
+        mode = mode.toUpperCase();
+        if(reservation.getNumberOfHours() * reservation.getSpot().getPricePerHour() > amountSent){
             throw new Exception("Insufficient Amount");
         }
-        mode=mode.toUpperCase();
-        if((!mode.equals(PaymentMode.CARD)) ||(!mode.equals(PaymentMode.UPI)) || (!mode.equals(PaymentMode.CASH))){
-            throw new Exception("Payment mode not detected");
-        }
-        if(mode.equals(PaymentMode.CARD)){
-            payment.setPaymentMode(PaymentMode.CARD);
-        }else if(mode.equals(PaymentMode.UPI)){
-            payment.setPaymentMode(PaymentMode.UPI);
-        }else if(mode.equals(PaymentMode.CASH)){
+        if(mode.equals(payment1)){
             payment.setPaymentMode(PaymentMode.CASH);
-        }else{
+        }
+        else if(mode.equals(payment2))  {
+            payment.setPaymentMode(PaymentMode.CARD);
+        }
+        else if (mode.equals(payment3)){
+            payment.setPaymentMode(PaymentMode.UPI);
+        }
+        else{
             throw new Exception("Payment mode not detected");
         }
         payment.setPaymentCompleted(true);
